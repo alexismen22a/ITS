d0ad28f30cd0efabf1d8d905402a0630d6bf854b
EmikoJenn <TabrisSoreth@gmail.com>
2023-11-26 17:55:04 -0600

    Fixed the audio functionality

--- main/app/page.tsx
+++ main/app/page.tsx
@@ -6,44 +6,44 @@ const buttonInfo = [
   {
     tittle: "Respuestas Básicas",
     img: "/assets/respuestas.png",
-    url: "/respuestas-basicas",
-    // music: "/assets/music.wav",
+    // url: "/respuestas-basicas",
+    audio: "/assets/music.wav",
   },
   {
     tittle: "Necesidades Físicas",
     img: "/assets/Nececidades.jpg",
     url: "/necesidades-fisicas",
-    // music: "/assets/music.wav",
+    // audio: "/assets/music.wav",
   },
   {
     tittle: "Confort y Bienestar",
     img: "/assets/confort.png",
     url: "/confort-bienestar",
-    // music: "/assets/music.wav",
+    // audio: "/assets/music.wav",
   },
   {
     tittle: "Emociones y Sentimientos",
     img: "/assets/sentimientos.PNG",
     url: "/emociones-sentimientos",
-    // music: "/assets/music.wav",
+    // audio: "/assets/music.wav",
   },
   {
     tittle: "Solicitudes Cotidianas",
     img: "/assets/Solocitudes cot.png",
     url: "/solicitudes-cotidianas",
-    // music: "/assets/music.wav",
+    // audio: "/assets/music.wav",
   },
   {
     tittle: "Actividades",
     img: "/assets/actividades.jpg",
     url: "/actividades",
-    // music: "/assets/music.wav",
+    // audio: "/assets/music.wav",
   },
   {
     tittle: "Interacción Social",
     img: "/assets/interacion social.jpg",
     url: "/interaccion-social",
-    // music: "/assets/music.wav",
+    // audio: "/assets/music.wav",
   },
 ];
 
